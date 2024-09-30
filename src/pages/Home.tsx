7ecbe7ef23bf969bc09063372738ea53f6b632dd
알노리 <67145096+ynng3@users.noreply.github.com>
2024-09-30 09:42:03 +0900

    Remove unnecessary module declarations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
 
 function Home() {
   return (
